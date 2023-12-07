74fe95de94275cd81f719cff4640fa37bf9a696b
alenabocharova2603 <alenabocharova2603@gmail.com>
2023-12-07 11:15:51 +0100

    init commit

---
+++ src/main/java/com/stepic/stepic_java/StepicJavaApplication.java
@@ -0,0 +1,13 @@
+package com.stepic.stepic_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepicJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepicJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stepic/stepic_java/StepicJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepic.stepic_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepicJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
